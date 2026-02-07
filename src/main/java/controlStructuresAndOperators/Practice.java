d1a35123d831de84c4f2e04bbd4c5f6960ccd288
Aleksei <morgachev_aleksey@mail.ru>
2026-02-08 01:33:57 +0300

    v.2 Добавлена теория по опереторам и управляющим конструкциям

--- src/main/java/controlStructuresAndOperators/Practice.java
+++ src/main/java/controlStructuresAndOperators/Practice.java
@@ -1,4 +1,9 @@
 package controlStructuresAndOperators;
 
 public class Practice {
+    public static void main(String[] args) {
+        //Тернарный оператор
+        int x = (true) ? 4 : 5;
+        System.out.println(x);
+    }
 }
